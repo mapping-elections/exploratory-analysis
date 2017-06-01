a6ac84adf8356f970f1bb38c614d53d78ab1170d
Lincoln Mullen <lincoln@lincolnmullen.com>
2017-05-31 22:02:55 -0400

    Sample map page

---
+++ sample-map-page/create.sh
@@ -0,0 +1,2 @@
+pandoc sample-map-show-1796.md --standalone --include-in-header includes.html -o sample-map-show-1796-from-pandoc.html
+pandoc sample-map-show-1798.md --standalone --include-in-header includes.html -o sample-map-show-1798-from-pandoc.html
